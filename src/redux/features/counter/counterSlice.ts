e0b7d998e7566e84309fc1308e69f021950ec6ca
shariar17e <shariarhossain23@gmail.com>
2024-01-02 11:34:15 +0600

    create counter slice

---
+++ src/redux/features/counter/counterSlice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface CounterInterface {
+  count: number;
+}
+const initialState: CounterInterface = {
+  count: 0,
+};
+const counterSlice = createSlice({
+  name: "coutner",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export default counterSlice;
