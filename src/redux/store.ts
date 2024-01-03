14baec1e660fca037c19276a6249bddc53d312f3
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-04 00:49:00 +0600

    added type on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,4 +4,7 @@ const store = configureStore({
   reducer: {},
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type appDispatch = typeof store.dispatch;
+
 export default store;
