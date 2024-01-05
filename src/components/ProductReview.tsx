727c36821a78492acee62ee7e729ee4d1da7d6d8
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-05 11:28:16 +0600

    implement mutaion on product details page

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -1,21 +1,49 @@
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
-import { Button } from './ui/button';
-import { Textarea } from './ui/textarea';
-import { FiSend } from 'react-icons/fi';
+import { useState } from "react";
+import { FiSend } from "react-icons/fi";
+import { useCommentPostMutation } from "../redux/features/api/apiSlice";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
 
 const dummyComments = [
-  'Bhalo na',
-  'Ki shob ghori egula??',
-  'Eta kono product holo ??',
-  '200 taka dibo, hobe ??',
+  "Bhalo na",
+  "Ki shob ghori egula??",
+  "Eta kono product holo ??",
+  "200 taka dibo, hobe ??",
 ];
 
-export default function ProductReview() {
+interface IProps {
+  id: string;
+}
+
+export default function ProductReview({ id }: IProps) {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const [postComment, options] = useCommentPostMutation();
+
+  console.log(options);
+
+  const handleComment = () => {
+    const option = {
+      id: id,
+      data: { comment: inputValue },
+    };
+    postComment(option);
+
+    setInputValue("");
+  };
   return (
     <div className="max-w-7xl mx-auto mt-5">
       <div className="flex gap-5 items-center">
-        <Textarea className="min-h-[30px]" />
-        <Button className="rounded-full h-10 w-10 p-2 text-[25px]">
+        <Textarea
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="min-h-[30px]"
+        />
+        <Button
+          onClick={() => handleComment()}
+          className="rounded-full h-10 w-10 p-2 text-[25px]"
+        >
           <FiSend />
         </Button>
       </div>
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -25,7 +25,7 @@ export default function ProductDetails() {
           <Button>Add to cart</Button>
         </div>
       </div>
-      <ProductReview />
+      <ProductReview id={product?._id} />
     </>
   );
 }
